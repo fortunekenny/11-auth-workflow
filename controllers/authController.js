23f1a26ac621e5bd63494dfbf80af79af42dba2e
fortunekenny <gbephraim@gmail.com>
2023-09-21 05:04:16 +0100

    controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,7 +37,26 @@ const register = async (req, res) => {
   // res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 
-const verifyEmail = async (req, res) => {};
+const verifyEmail = async (req, res) => {
+  const { verificationToken, email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("Verification Failed");
+  }
+
+  if (user.verificationToken !== verificationToken) {
+    throw new CustomError.UnauthenticatedError("Verification Failed");
+  }
+
+  (user.isVerified = true), (user.verified = Date.now());
+  user.verificationToken = "";
+
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: "email verified" });
+};
 
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -78,4 +97,5 @@ module.exports = {
   register,
   login,
   logout,
+  verifyEmail,
 };
