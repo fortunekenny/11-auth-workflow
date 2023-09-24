b3578fa8027776109a2de55616bc6d79e9074681
fortunekenny <gbephraim@gmail.com>
2023-09-24 05:21:38 +0100

    token

--- utils/nodeMailerConfig.js
+++ utils/nodeMailerConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   host: "smtp.ethereal.email",
   port: 587,
   auth: {
-    user: "clinton.mann@ethereal.email",
-    pass: "X695p5dBabpAprsxhT",
+    user: "brigitte.rath46@ethereal.email",
+    pass: "cKP836Q6SzyZWTyw9J",
   },
 };
